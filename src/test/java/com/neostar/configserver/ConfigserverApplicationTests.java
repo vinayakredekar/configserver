b91860a0efc566ba67ced664e40223fbff3d6cad
Vinayak Redekar <redekar62.vinayak@gmail.com>
2025-10-04 13:20:21 +0800

    First Commit

---
+++ src/test/java/com/neostar/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neostar.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
